bea4ca85a995de8806a003fbf1971dd0f168a146
liqiang2 <spacecode@qq.com>
2022-04-15 21:42:49 +0800

    评论输入框调整

--- cloudfunctions/getComments/index.js
+++ cloudfunctions/getComments/index.js
@@ -1,31 +1,34 @@
-const cloud = require('wx-server-sdk')
+const cloud = require("wx-server-sdk");
 cloud.init({
-  env: cloud.DYNAMIC_CURRENT_ENV
-})
-var db = cloud.database()
-var $ = db.command.aggregate
+  env: cloud.DYNAMIC_CURRENT_ENV,
+});
+var db = cloud.database();
+var $ = db.command.aggregate;
 exports.main = async (event, context) => {
-    return db.collection("comments").aggregate()
-            .lookup({
-              from:"user",
-              localField: 'from',
-              foreignField: '_id',
-              as: 'userInfo'
-            })
-          .replaceRoot({  
-            newRoot: $.mergeObjects([$.arrayElemAt(['$userInfo', 0]), '$$ROOT'])
-          })
-          .project({
-            userInfo: 0
-          }).match({
-            course:event.course
-          })
-            .end({
-              success:function(res){
-                return res;
-              },
-              fail(error) {
-                return error;
-              }
-            })
-  }
+  return db
+    .collection("comments")
+    .aggregate()
+    .lookup({
+      from: "user",
+      localField: "from",
+      foreignField: "_id",
+      as: "userInfo",
+    })
+    .replaceRoot({
+      newRoot: $.mergeObjects([$.arrayElemAt(["$userInfo", 0]), "$$ROOT"]),
+    })
+    .project({
+      userInfo: 0,
+    })
+    .match({
+      course: event.course,
+    })
+    .end({
+      success: function (res) {
+        return res;
+      },
+      fail(error) {
+        return error;
+      },
+    });
+};
--- miniprogram/components/comments/index.js
+++ miniprogram/components/comments/index.js
@@ -1,34 +1,81 @@
-const db=wx.cloud.database()
+const db = wx.cloud.database();
 Component({
-    options:{
-        addGlobalClass:true
-    },
-    properties: {
-        commentList:Array
-    },
-    data: {
-    },
-    methods: {
-        comment(e){
-            console.log(e.currentTarget.dataset.item)
-            console.log('二级下标',e.currentTarget.dataset.cindex)
-            console.log('一级下标',e.currentTarget.dataset.index)
-            this.setData({
-                // commentList[index].push()
+  options: {
+    addGlobalClass: true,
+  },
+  properties: {
+    commentList: Array,
+  },
+  data: {
+    inputKey: "",
+    bottom: 0,
+    copyBottom: 0,
+    tips: "有爱评论，说点好听的～",
+  },
+  methods: {
+    sendComment(id) {
+      id ='course00001'
+      let content= this.data.inputKey
+      if(content !==''|| content !==null){
+        db.collection("comments").add({
+          data: {
+            course: 'course00001',
+            content,
+            like: 0,
+            time: db.serverDate(),
+            from: "636050766258033305ba63891ef8a0d6",
+          },
+          success: (res) => {
+            this.triggerEvent('getCommment', {id: id});
+            this.dataset({
+              inputKey:''
             })
-        },
-        goToUser(e){
-            console.log('跳转至'+e.currentTarget.dataset.name +'的资料页')
-        },
-        ljc(){
-            console.log(1)
-        },
-        showMore(e){
-            console.log(1)
-            // console.log(e.currentTarget.dataset.index)
-        },
-        changeTime(val){
-            console.log(val)
-        }
-    }
-})
+          }
+        });
+      }
+    },
+    comment(e) {
+      console.log(e.currentTarget.dataset.item);
+      console.log("二级下标", e.currentTarget.dataset.cindex);
+      console.log("一级下标", e.currentTarget.dataset.index);
+      let nickName = "回复 @" + e.currentTarget.dataset.item.nickName;
+      this.focus1();
+      this.setData({
+        focus: true,
+        tips: nickName,
+      });
+    },
+    goToUser(e) {
+      console.log("跳转至" + e.currentTarget.dataset.name + "的资料页");
+    },
+    showMore(e) {
+      console.log(1);
+    },
+    getInput(e) {
+      this.setData({
+        inputKey: e.detail.value,
+      });
+    },
+    focus(e) {
+      let height = e.detail.height;
+      let copy = this.data.copyBottom;
+      if (height !== copy) {
+        this.setData({
+          bottom: height,
+          copyBottom: height,
+        });
+      }
+    },
+    focus1() {
+      this.setData({
+        bottom: this.data.copyBottom,
+      });
+    },
+    blur() {
+      this.setData({
+        bottom: 0,
+        tips: "有爱评论，说点好听的～",
+      });
+    },
+  },
+});
